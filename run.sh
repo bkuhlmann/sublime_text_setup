72adb68c85419cacc50aca72173c5a4e9d94eefe
Brooke Kuhlmann <brooke@alchemists.io>
2015-04-27 20:24:24 -0600

    Updated to Sublime Text 3.

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ set -o nounset # Exit, with error message, when attempting to use an undefined v
 set -o errexit # Abort script at first error, when a command exits with non-zero status.
 set -o pipefail # Returns exit status of the last command in the pipe that returned a non-zero return value.
 IFS=$'\n\t' # Defines how Bash splits words and iterates arrays. This defines newlines and tabs as delimiters.
-SUBLIME_TEXT_ROOT="$HOME/Library/Application Support/Sublime Text 2"
+SUBLIME_TEXT_ROOT="$HOME/Library/Application Support/Sublime Text 3"
 
 # FUNCTIONS
 source scripts/utilities.sh
